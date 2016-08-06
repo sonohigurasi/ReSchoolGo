dd4555b9cfd291c6efb52781e9384239c1f13363
ragrog <ragrog216@outlook.jp>
2016-08-06 23:23:55 +0900

    新規作成.Re

---
+++ Assets/GameStartUIMgr.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameStartUIMgr : MonoBehaviour
+{
+    public Text SubjectUI;
+    public Text timeUI;
+    public Text descriptionUI;
+
+    public void SetSubject(string subject_name)
+    {
+        SubjectUI.text = "科目名　　　" + subject_name;
+    }
+    public void SetTimeUI(float time)
+    {
+         timeUI.text = "クエスト開始期限残り   " + ((int)time / 60).ToString("D2") + ":" + (time % 60).ToString("00.00");
+        //timeUI.text = "fdsafdasfdsafdsafdas";
+
+    }
+    public void SetDescription(string description)
+    {
+        descriptionUI.text = description;
+
+    }
+}
---
+++ Assets/StartSceneManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartSceneManager : MonoBehaviour {
+
+   GameStartUIMgr uimanager;
+    public float start_time  = 82800;
+    public float span;
+
+    float time;
+	// Use this for initialization
+	void Start () {
+        span = start_time -(System.DateTime.Now.Hour * 60 * 60 + System.DateTime.Now.Minute * 60 + System.DateTime.Now.Second);
+        uimanager = this.GetComponent<GameStartUIMgr>();
+        uimanager.SetSubject("解析学");
+        uimanager.SetDescription("ゴミ授業");
+        
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        span -= Time.deltaTime;
+        uimanager.SetTimeUI(span);
+    }
+}
