6e8f275cfc5a20f6a8cca5f3a6c8431139d7dc57
zonozono32 <zonocat32@gmail.com>
2016-08-07 00:36:08 +0900

    [add]subjectNumberを指定してRecordを取得できるメソッドの追加

--- Assets/Scripts/Database/Database.cs
+++ Assets/Scripts/Database/Database.cs
@@ -47,6 +47,31 @@ public class Database {
         return records;
     }
 
+    static public SubjectDataRecord[] getRecordFromSubjectTableBySubjectNumber(int number)
+    {
+        // Select
+        string selectQuery = "select * from SubjectTable where Number = " + number;
+        DataTable dataTable = openDatabase().ExecuteQuery(selectQuery);
+
+        SubjectDataRecord[] records = new SubjectDataRecord[dataTable.Rows.Count];
+        int count = 0;
+        foreach (DataRow dr in dataTable.Rows)
+        {
+            SubjectDataRecord record;
+
+            record.number = (int)dr["Number"];
+            record.name = (string)dr["Name"];
+            record.date = (int)dr["Date"];
+            record.detail = (string)dr["Detail"];
+
+            records[count] = record;
+
+            count++;
+        }
+
+        return records;
+    }
+
     static public PastQuestionRecord[] getAllRecordFromQuestionTable()
     {
         // Select
