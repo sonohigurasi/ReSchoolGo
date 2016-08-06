09587371a90440f1c6e3d022616e73100922f048
zonozono32 <zonocat32@gmail.com>
2016-08-06 23:49:34 +0900

    [add]中園の作成分の追加

---
+++ Assets/Scripts/Database/Database.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public struct SubjectDataRecord
+{
+    public int number; 
+    public string name;
+    public float date;
+    public string detail;
+}
+
+public struct PastQuestionRecord
+{
+    public int number;
+    public string imageName;
+}
+
+public class Database {
+
+	static SqliteDatabase openDatabase()
+    {
+        return new SqliteDatabase("database.db");
+    }
+
+    static public SubjectDataRecord[] getAllRecordFromSubjectTable()
+    {
+        // Select
+        string selectQuery = "select * from SubjectTable";
+        DataTable dataTable = openDatabase().ExecuteQuery(selectQuery);
+
+        SubjectDataRecord[] records = new SubjectDataRecord[dataTable.Rows.Count];
+        int count = 0;
+        foreach (DataRow dr in dataTable.Rows)
+        {
+            SubjectDataRecord record;
+
+            record.number = (int)dr["Number"];
+            record.name = (string)dr["Name"];
+            record.date = (int)dr["Date"];
+            record.detail = (string)dr["Detail"];
+
+            records[count] = record;
+
+            count++;
+        }
+
+        return records;
+    }
+
+    static public PastQuestionRecord[] getAllRecordFromQuestionTable()
+    {
+        // Select
+        string selectQuery = "select * from PastQuestionTable";
+        DataTable dataTable = openDatabase().ExecuteQuery(selectQuery);
+
+        PastQuestionRecord[] records = new PastQuestionRecord[dataTable.Rows.Count];
+        int count = 0;
+        foreach (DataRow dr in dataTable.Rows)
+        {
+            PastQuestionRecord record;
+
+            record.number = (int)dr["Number"];
+            record.imageName = (string)dr["ImageName"];
+
+            records[count] = record;
+
+            count++;
+        }
+
+        return records;
+    }
+
+    static public PastQuestionRecord[] getRecordFromQuestionTableBySubjectNumber(int number)
+    {
+        // Select
+        string selectQuery = "select * from PastQuestionTable inner join SubjectRelationTable on PastQuestionTable.Number = SubjectRelationTable.QuestionNumber where SubjectNumber = " + number;
+        DataTable dataTable = openDatabase().ExecuteQuery(selectQuery);
+
+        PastQuestionRecord[] records = new PastQuestionRecord[dataTable.Rows.Count];
+        int count = 0;
+        foreach (DataRow dr in dataTable.Rows)
+        {
+            PastQuestionRecord record;
+
+            record.number = (int)dr["Number"];
+            record.imageName = (string)dr["ImageName"];
+
+            records[count] = record;
+
+            count++;
+        }
+
+        return records;
+    }
+}
---
+++ Assets/Scripts/ScrollController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScrollController : MonoBehaviour {
+    
+    [SerializeField]
+    RectTransform prefab = null;
+
+    void Start()
+    {
+        SubjectDataRecord[] test = Database.getAllRecordFromSubjectTable();
+
+        for (int i = 0; i < test.Length; i++)
+        {
+            var item = GameObject.Instantiate(prefab) as RectTransform;
+            item.SetParent(transform, false);
+
+            var text = item.GetComponentInChildren<Text>();
+            text.text = "date:" + test[i].date;
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
