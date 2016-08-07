6a4daad182ba1960b4f024180ec5ff65cc3ea297
zonozono32 <zonocat32@gmail.com>
2016-08-07 10:49:11 +0900

    [add]ユーザデータ管理クラスの追加

---
+++ Assets/Scripts/UserManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UserManager : Singleton {
+    const string kIsQuestOrder = "OrderKey";
+    const string kSelectQuestNumber = "SelectNumberKey";
+    const string kClearCount = "ClearKey";
+
+    public bool isQuestOrder;
+    public int selectQuestNumber;
+    public int clearCount;
+
+	// Use this for initialization
+	void Start () {
+        isQuestOrder = bool.Parse(PlayerPrefs.GetString(kIsQuestOrder, "false"));
+        selectQuestNumber = PlayerPrefs.GetInt(kSelectQuestNumber, 0);
+        clearCount = PlayerPrefs.GetInt(kClearCount, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
