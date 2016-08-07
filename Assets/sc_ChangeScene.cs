3aee20343699322749204d9131401c904862bc94
130441002-PC\Akita <aa@gmail.com>
2016-08-07 10:12:36 +0900

    過去問を追加 スクリプトの見直し

---
+++ Assets/sc_ChangeScene.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class sc_ChangeScene : MonoBehaviour {
+    public string sceneName;
+
+    public void changeScene()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
---
+++ Assets/sc_image.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class sc_image : MonoBehaviour {
+    Image img;
+    public Sprite[] image;
+    int id = 1;
+
+    void Awake()
+    {
+        Debug.Log(image.Length);
+        
+        // IDを取得する
+        //id = TestSingleton.test.id;
+        Texture2D texture = Resources.Load("question" + id) as Texture2D;
+        img = GameObject.Find("Canvas/Panel/image").GetComponent<Image>();
+        Debug.Log(id);
+        img.sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), Vector2.zero);
+
+    }
+}
