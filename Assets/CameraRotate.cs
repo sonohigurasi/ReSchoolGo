730a4cd7e585bb03c1cae8ffc63772c84cdab942
Kuwabara Kousuke <t314023@st.chukyo-u.ac.jp>
2016-08-07 14:44:19 +0900

    [add]端末のジャイロを拾ってカメラを回転するように変更

---
+++ Assets/CameraRotate.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraRotate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Input.gyro.enabled = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Quaternion gattitude = Input.gyro.attitude;
+        gattitude.x *= -1;
+        gattitude.y *= -1;
+        var rot =
+            Quaternion.Euler(90, 0, 0) * gattitude;
+        this.transform.Rotate(0, (rot.y * 1), 0);
+	}
+
+    void OnDestroy() {
+        Input.gyro.enabled = false;
+    }
+}
